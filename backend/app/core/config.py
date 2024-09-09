c59b748f44f89c35fddf20141ed126984cabb7df
emon535 <emon535@gmail.com>
2024-09-09 18:57:06 +0100

    database renamed

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///.//test.db"
+    DATABASE_URL: str = "sqlite:///database.db"
 
 settings = Settings()
